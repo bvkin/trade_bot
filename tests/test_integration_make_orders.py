a428819275781e4e36541333b50fbc30a76f65e9
Ben Kincaid <bkincaid1@protonmail.com>
2023-10-05 09:39:21 -0400

    add integration test for make_orders (#18)
    ---------
    
    Co-authored-by: Benjamin Kincaid <ben.kincaid@rocketinsights.com>

---
+++ tests/test_integration_make_orders.py
@@ -0,0 +1,48 @@
+from pandas import DataFrame
+import pytest
+from unittest.mock import call, MagicMock, patch
+from .test_data import moving_averages_test_cases
+from trade_bot.trading import make_orders, BEARISH, BULLISH, NO_CLEAR_PATTERN
+
+tickers = [case["ticker"] for case in moving_averages_test_cases]
+close_prices = [case["close_prices"] for case in moving_averages_test_cases]
+
+
+def price_data_generator(values):
+    """
+    Generates a fresh dataframe each time called for mocking
+    """
+    while True:
+        for value in values:
+            df = DataFrame({"close" : value})
+            yield df
+
+@pytest.fixture
+def mock_trade_manager():
+    mock = MagicMock()
+    mock.get_owned_tickers.return_value = tickers
+    mock.get_price_data.side_effect = price_data_generator(close_prices)
+    return mock
+
+@pytest.fixture
+def mock_sns_client():
+    return MagicMock()
+
+@patch("trade_bot.trading.tickers_sp500", return_value=tickers)
+@patch("trade_bot.trading.get_first_last_market_days", return_value=("2023-01-01", "2023-01-21"))
+def test_make_orders(mock_get_days, mock_tickers_sp500, mock_trade_manager, mock_sns_client):
+
+    mock_sns_topic_arn = "arn:aws:sns:us-east-1:123456789101:trade_bot_signals"
+    make_orders(trade_manager=mock_trade_manager, sns_client=mock_sns_client, sns_topic_arn=mock_sns_topic_arn)
+    
+    # Ensure buy_stock was called for BULLISH signal
+    mock_trade_manager.buy_stock.assert_called_once_with("GOOGL")
+    
+    # Ensure sell_stock was called for BEARISH signal
+    mock_trade_manager.sell_stock.assert_called_once_with("AAPL")
+    
+    # Ensure SNS publish was called for both buy and sell
+    mock_sns_client.publish.assert_has_calls([
+        call(Message='Trade Bot Buy Orders Made: GOOGL',TopicArn=mock_sns_topic_arn),
+        call(Message='Trade Bot Sell Orders Made: AAPL', TopicArn=mock_sns_topic_arn)
+    ])
