29b107fb8f79608288940692edab74c6c0b30fd6
Brandon Bell <brandonbell@macbook-air.mynetworksettings.com>
2023-12-13 20:05:01 -0500

    push working

--- trade_bot/make_orders.py
+++ trade_bot/make_orders.py
@@ -40,9 +40,51 @@ def make_orders(trade_manager: AlpacaTradeManager, Strategy: Strategy, tickers: 
         logging.info("Evaluating " + ticker + " for buy")
         df = trade_manager.get_price_data(ticker, period_start, period_end)
         strat = Strategy(df)
+def make_orders(trade_manager: AlpacaTradeManager, Strategy: Strategy, tickers: List[str], sns_client: SNSClient,  sns_topic_arn: str = None) -> None:
+    """
+    Makes buy orders for all stocks in the S&P 500 given a bullish signal.
+    Makes sell orders for all owned stocks bearish signal.
+    """
+    
+    purchased_tickers, sold_tickers = [], []
+    period_start, period_end = get_market_day_range(21) # 21 for 20 day moving avg
+
+    logging.info("Making orders...")
+    
+    owned_tickers = trade_manager.get_owned_tickers()
+
+    for ticker in owned_tickers:
+        logging.info("Evaluating " + ticker + " for sell")
+        df = trade_manager.get_price_data(ticker, period_start, period_end)
+        
+        strat = Strategy(df)
+        
+        if strat.signal() == TradeSignal.BEARISH:
+            trade_manager.sell_stock(ticker)
+            logging.info("Sell order for " + ticker + " placed.")
+            sold_tickers.append(ticker)
+
+    if sold_tickers and sns_topic_arn != None:
+        sns_client.publish(Message=f'Trade Bot Sell Orders Made: {", ".join(sold_tickers)}', TopicArn=sns_topic_arn)
+    
+    balance_available = trade_manager.api.get_account().buying_power
+
+    for ticker in tickers:
+        logging.info("Evaluating " + ticker + " for buy")
+        df = trade_manager.get_price_data(ticker, period_start, period_end)
+        strat = Strategy(df)
+
+        trade_manager.buy_stock(ticker)
 
         if strat.signal() == TradeSignal.BULLISH:
             trade_manager.buy_stock(ticker)
+            
+            purchased_tickers.append(ticker)
+
+    if purchased_tickers and sns_topic_arn != None:
+        sns_client.publish(Message=f'Trade Bot Buy Orders Made: {", ".join(purchased_tickers)}', TopicArn=sns_topic_arn)
+
+        if strat.signal() == TradeSignal.BULLISH:
             
             purchased_tickers.append(ticker)
 
